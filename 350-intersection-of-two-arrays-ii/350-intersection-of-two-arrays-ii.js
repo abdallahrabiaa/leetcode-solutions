49221ff49074cb169f75c3be4ba31be32e476291
abdallahrabiaa <64704468+abdallahrabiaa@users.noreply.github.com>
2022-05-18 15:10:04 +0200

    Time: 72 ms (77.20%), Space: 44.3 MB (31.00%) - LeetHub

---
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+    let cache = {};
+    let result=[];
+    for (let num of nums2){
+        if (!cache[num])cache[num]=1;
+        else cache[num]++;
+    }
+    for (let num of nums1){
+        if (cache[num]){
+            result.push(num);
+            cache[num]--;
+        }
+    }
+    
+    return result ;
+};
